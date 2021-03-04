a48e428d4244583259aa9c99a930fc379a6b0d5a
Eakkaphol Saehiang <eakkaphol.s@hotmail.com>
2021-03-04 20:27:08 +0700

    revised index.js (test)

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-const express = require("express"),
-  app = express(),
-  config = require("./configs/app");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
 
-//Express Configs
-require("./configs/express")(app);
+app.use(express.static("public"));
 
-//Routes
-app.use(require("./routes"));
-
-//API test
-app.get("/test1", (req, res) => {
+app.get("/", (req, res) => {
   console.log("get /");
   res.send("/ It works!");
 });
 
-app.get("/test2", function (req, res) {
-  console.log("get /test");
-  res.send("/test It works!");
-});
-
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: "It works! test",
-  });
-});
-
-// Start Server
-const server = app.listen(config.port, () => {
-  let host = server.address().address;
-  let port = server.address().port;
-  console.log(`Server is running at http://${host}:${port}`);
-  console.log(`Server is running in ${process.env.NODE_ENV} mode`);
-});
+app.listen(port);
