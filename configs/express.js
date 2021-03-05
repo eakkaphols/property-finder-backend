17e1e707ba20e84ef14c101fb72fc49289703709
Eakkaphol Saehiang <eakkaphol.s@hotmail.com>
2021-03-05 21:38:23 +0700

    allow allowedOrigins heroku

--- configs/express.js
+++ configs/express.js
@@ -30,21 +30,21 @@ module.exports = async (app) => {
   // Connect MongoDB
   require("../configs/databases");
 
-  // // CORS
-  // const allowedOrigins = [];
-  // const corsOptions = {
-  //   origin: function (origin, callback) {
-  //     if (!origin) return callback(null, true);
-  //     if (allowedOrigins.indexOf(origin) === -1) {
-  //       const msg =
-  //         "The CORS policy for this site does not " +
-  //         "allow access from the specified Origin";
-  //       return callback(new Error(msg), false);
-  //     }
-  //     return callback(null, true);
-  //   },
-  // };
-  // app.use(cors(corsOptions));
+  // CORS
+  const allowedOrigins = ["https://xlan-property-finder.herokuapp.com"];
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  };
+  app.use(cors(corsOptions));
 
   //Paser body
   app.use(express.json());
