143f409d0d73e550455b0803a86529fbe03429de
Eakkaphol Saehiang <eakkaphol.s@hotmail.com>
2021-03-13 09:49:52 +0700

    revised socket.io controller

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -45,10 +45,10 @@ const methods = {
     try {
       let result = await Service.findByPostedId(req, req.params.id);
 
-      const postUserSocket = req.connectedUsers[req.params.id];
+      const myPropertySocket = req.connectedUsers[req.params.id];
 
-      if (postUserSocket) {
-        req.io.to(postUserSocket).emit("post_response", result);
+      if (myPropertySocket) {
+        req.io.to(myPropertySocket).emit("myproperty_response", result);
       }
 
       res.status(200).json(result);
@@ -62,6 +62,15 @@ const methods = {
     try {
       //const result = await Service.insert(req.body);
       const result = await Service.insertWithImages(req.body, req.files);
+
+
+      let rsSocket = await Service.findByPostedId(req, result.postedBy);
+      const myPropertySocket = req.connectedUsers[(req, result.postedBy)];
+      if (myPropertySocket) {
+        req.io.to(myPropertySocket).emit("myproperty_response", rsSocket);
+      }
+
+
       res.status(201).json(result);
     } catch (error) {
       res
@@ -100,19 +109,28 @@ const methods = {
         .json({ status: error.status, message: error.message });
     }
   },
+  async onApproval(req, res) {
+    try {
+      const result = await Service.approval(req.params.postedby);
 
-  //   async onLogin(req, res) {
-  //     try {
-  //       const result = await Service.login(req.body);
-  //       res.success(result);
-  //     } catch (error) {
-  //       res.error(error);
-  //     }
-  //   },
+      // const postingUserSocket = req.connectedUsers[req.params.postedby];
+      // if (postingUserSocket) {
+      //   req.io.to(postingUserSocket).emit("postapproval_response", result);
+      // }
 
-  //   onRegister(req, res) {
-  //     res.success({ page: "login" });
-  //   },
+      let rsSocket = await Service.findByPostedId(req, req.body.postedBy);
+      const myPropertySocket = req.connectedUsers[(req, req.body.postedBy)];
+      if (myPropertySocket) {
+        req.io.to(myPropertySocket).emit("myproperty_response", rsSocket);
+      }
+
+      res.status(201).json(result);
+    } catch (error) {
+      res
+        .status(error.status)
+        .json({ status: error.status, message: error.message });
+    }
+  },
 };
 
 module.exports = { ...methods };
